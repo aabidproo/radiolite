1aa3e5e35e458f240e1f3f1757037c34052b7aef
aabidproo <aabidhasan495@gmail.com>
2026-01-20 05:15:04 +0545

    Fix NameError: Import HTMLResponse in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import os
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.ext.asyncio import AsyncSession
